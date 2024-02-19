60e06b4a0d6ae02e8fa6a21c8308391a40cc2358
Piotr <khazal@wp.pl>
2024-02-19 11:12:28 +0100

    interceptor active

--- src/shared/interceptors/logger.interceptor.ts
+++ src/shared/interceptors/logger.interceptor.ts
@@ -10,15 +10,23 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('***');
+    const url = context.getArgs()[0].url;
+    const method = context.getArgs()[0].method;
+    const httpContext = context.switchToHttp();
+    const response = httpContext.getResponse();
+    console.log('================================');
+    console.log(`${method} ${url} `);
     console.log(`Start request in ${context.getClass().name}`);
 
     const start = Date.now();
 
     return next.handle().pipe(
       tap(() => {
-        console.log(`Request ended in: ${Date.now() - start}ms`);
-        console.log('***');
+        const statusCode = response.statusCode;
+        console.log(
+          `Request ended with status ${statusCode} in: ${Date.now() - start}ms`,
+        );
+        console.log('================================');
       }),
     );
   }
